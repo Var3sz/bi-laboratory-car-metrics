612c245b430ad2e5e03b0ea9439691c68b71b2a7
Var3sz <varga.adam011208@gmail.com>
2025-05-17 00:20:40 +0200

    feat: ETL phase 2 - cleaning and staging

---
+++ SQL scripts/Create_Staged_CarSales.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[staged.CarSales](
+	[date] [date] NULL,
+	[city] [nvarchar](1000) NULL,
+	[salesperson] [nvarchar](1000) NULL,
+	[customername] [nvarchar](1000) NULL,
+	[carmake] [nvarchar](1000) NULL,
+	[carmodel] [nvarchar](1000) NULL,
+	[caryear] [int] NULL,
+	[saleprice] [int] NULL,
+	[commissionrate] [float] NULL,
+	[commisionearned] [float] NULL,
+	[profit] [float] NULL,
+)
+
---
+++ SQL scripts/Create_Staged_CityData.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[staged.CityData](
+	[city] [nvarchar](1000) NULL,
+	[lat] [float] NULL,
+	[lng] [float] NULL,
+	[country] [nvarchar](1000) NULL,
+	[population] [int] NULL,
+)
+
+
